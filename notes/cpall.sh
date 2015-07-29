727577f5f3379a6ec3b963ae8d72675054c122e0
totzYuta <yuta.totz@gmail.com>
2015-07-29 10:30:30 +0900

    Split the notes

--- notes/cpall.sh
+++ notes/cpall.sh
@@ -1,7 +1,8 @@
+echo "# lib5\n" >> ~/workspace/college/func-programming/notes/all5.md
 for file in *; do    
-  echo ${file} >> ~/workspace/college/func-programming/notes/all.md
-  echo "\n" >> ~/workspace/college/func-programming/notes/all.md
-  echo "\`\`\`\n" >> ~/workspace/college/func-programming/notes/all.md
-  cat "${file}" >> ~/workspace/college/func-programming/notes/all.md
-  echo "\`\`\`\n" >> ~/workspace/college/func-programming/notes/all.md
+  echo ${file} >> ~/workspace/college/func-programming/notes/all5.md
+  echo "\n" >> ~/workspace/college/func-programming/notes/all5.md
+  echo "\`\`\`\n" >> ~/workspace/college/func-programming/notes/all5.md
+  cat "${file}" >> ~/workspace/college/func-programming/notes/all5.md
+  echo "\`\`\`\n" >> ~/workspace/college/func-programming/notes/all5.md
 done
